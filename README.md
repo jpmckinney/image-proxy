6585e784b9bd8a6419b807c79b5210fbf5d20861
James McKinney <james@slashpoundbang.com>
2013-02-16 18:33:14 -0500

    write image proxy

---
+++ README.md
@@ -0,0 +1,28 @@
+# Image Proxy
+
+A simple app based on mySociety's [connect-image-proxy](https://github.com/mysociety/node-connect-image-proxy) middleware for proxying and manipulating images.
+
+## Getting Started
+
+    npm install
+    node web.js
+
+## Deployment
+
+### Heroku
+
+    git clone https://github.com/opennorth/image-proxy.git
+    heroku create
+    heroku config:set NODE_ENV=production
+    git push heroku master
+    heroku apps:open
+
+## Bugs? Questions?
+
+This project's main repository is on GitHub: [http://github.com/opennorth/image-proxy](http://github.com/opennorth/image-proxy), where your contributions, forks, bug reports, feature requests, and feedback are greatly welcomed.
+
+## Acknowledgements
+
+This project is developed by [Open North](http://www.opennorth.ca/) through a partnership with the [Participatory Politics Foundation](http://www.participatorypolitics.org/).
+
+Copyright (c) 2013 Open North Inc., released under the MIT license
---
+++ web.js
@@ -0,0 +1,79 @@
+// @see https://devcenter.heroku.com/articles/nodejs#write-your-app
+var express = require('express')
+  , fs      = require('fs') // node
+  , gm      = require('gm')
+  , http    = require('http') // node
+  , https   = require('https') // node
+  , mime    = require('mime') // express
+  , url     = require('url') // node
+  , app = express.createServer(express.logger())
+  , mimeTypes = [
+    'image/jpeg',
+    'image/png',
+    'image/gif',
+    'image/jpg',
+  ];
+
+app.get('/', function (req, res, next) {
+  //@todo Allow requests from authorized hosts only.
+
+  var remote = req.param('url')
+    , width = req.param('width')
+    , height = req.param('height');
+
+  // Require query string parameters.
+  if (!remote || !width || !height) {
+    return res.send('Expected url, width and height query string parameters', 404);
+  }
+
+  // Validate query string parameters.
+  var parts = url.parse(remote);
+  // @see http://nodejs.org/api/url.html#url_url
+  if (['https:', 'http:'].indexOf(parts['protocol']) === -1) {
+    return res.send('Expected url to use the HTTP or HTTPS protocol', 404);
+  }
+  if (!parts['hostname']) {
+    return res.send('Expected url to include a hostname', 404);
+  }
+  if (isNaN(parseFloat(width))) {
+    return res.send('Expected width to be an integer', 404);
+  }
+  if (isNaN(parseFloat(height))) {
+    return res.send('Expected height to be an integer', 404);
+  }
+
+  var agent = parts['protocol'] === 'http:' ? http : https;
+
+  // @see http://nodejs.org/api/http.html#http_http_get_options_callback
+  agent.get(remote, function (res2) {
+    // The remote image must return status code 200.
+    // @see http://nodejs.org/api/http.html#http_response_statuscode
+    if (res2.statusCode !== 200) {
+      return res.send('Expected response code 200, got ' + res2.statusCode, 404);
+    }
+
+    // The remote image must be a valid content type.
+    // @see http://nodejs.org/api/http.html#http_request_headers
+    var mimeType = res2.headers['content-type'];
+    if (mimeTypes.indexOf(mimeType) === -1) {
+      return res.send('Expected content type ' + mimeTypes.join(', ') + ', got ' + mimeType, 404);
+    }
+
+    // @see https://github.com/aheckmann/gm#constructor
+    gm(res2, 'image.' + mime.extension(mimeType))
+    // @see http://www.imagemagick.org/Usage/thumbnails/#cut
+    .resize(width, height + '^>')
+    .gravity('Center')
+    .extent(width, height)
+    .stream(function (err, stdout, stderr) {
+      if (err) return next(err);
+      res.writeHead(200, {'Content-Type': mimeType});
+      stdout.pipe(res);
+    });
+  }).on('error', next);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log('Listening on ' + port);
+});
