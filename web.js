84480ae5d0bc6e076d6fbae0c410066a98762191
James McKinney <james@slashpoundbang.com>
2013-02-16 22:52:29 -0500

    add 5s timeout

--- web.js
+++ web.js
@@ -30,44 +30,48 @@ app.get('/:url/:width/:height', function (req, res, next) {
         return res.send('Expected url to include a hostname', 404);
       }
 
-      var agent = parts['protocol'] === 'http:' ? http : https;
+      var agent = parts['protocol'] === 'http:' ? http : https,
+        // @see http://nodejs.org/api/http.html#http_http_get_options_callback
+        , request = agent.get(remote, function (res2) {
+          // @see http://nodejs.org/api/http.html#http_response_statuscode
+          if ([301, 302].indexOf(res2.statusCode) !== -1 && res2.headers['location']) {
+            return retrieve(res2.headers['location']);
+          }
 
-      // @see http://nodejs.org/api/http.html#http_http_get_options_callback
-      agent.get(remote, function (res2) {
-        // @see http://nodejs.org/api/http.html#http_response_statuscode
-        if ([301, 302].indexOf(res2.statusCode) !== -1 && res2.headers['location']) {
-          return retrieve(res2.headers['location']);
-        }
+          // The remote image must return status code 200.
+          if (res2.statusCode !== 200) {
+            return res.send('Expected response code 200, got ' + res2.statusCode, 404);
+          }
 
-        // The remote image must return status code 200.
-        if (res2.statusCode !== 200) {
-          return res.send('Expected response code 200, got ' + res2.statusCode, 404);
-        }
+          // The remote image must be a valid content type.
+          // @see http://nodejs.org/api/http.html#http_request_headers
+          var mimeType = res2.headers['content-type'].replace(/;.+/, '');
+          if (mimeTypes.indexOf(mimeType) === -1) {
+            return res.send('Expected content type ' + mimeTypes.join(', ') + ', got ' + mimeType, 404);
+          }
 
-        // The remote image must be a valid content type.
-        // @see http://nodejs.org/api/http.html#http_request_headers
-        var mimeType = res2.headers['content-type'].replace(/;.+/, '');
-        if (mimeTypes.indexOf(mimeType) === -1) {
-          return res.send('Expected content type ' + mimeTypes.join(', ') + ', got ' + mimeType, 404);
-        }
-
-        // @see https://github.com/aheckmann/gm#constructor
-        imageMagick(res2, 'image.' + mime.extension(mimeType))
-        // @see http://www.imagemagick.org/Usage/thumbnails/#cut
-        .resize(width, height + '^>')
-        .gravity('Center')
-        .extent(width, height)
-        .stream(function (err, stdout, stderr) {
-          if (err) return next(err);
-          stderr.pipe(process.stderr);
-          // @see http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/Expiration.html
-          res.writeHead(200, {
-            'Content-Type': mimeType,
-            'Cache-Control': 'max-age=31536000, public', // 1 year
+          // @see https://github.com/aheckmann/gm#constructor
+          imageMagick(res2, 'image.' + mime.extension(mimeType))
+          // @see http://www.imagemagick.org/Usage/thumbnails/#cut
+          .resize(width, height + '^>')
+          .gravity('Center')
+          .extent(width, height)
+          .stream(function (err, stdout, stderr) {
+            if (err) return next(err);
+            stderr.pipe(process.stderr);
+            // @see http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/Expiration.html
+            res.writeHead(200, {
+              'Content-Type': mimeType,
+              'Cache-Control': 'max-age=31536000, public', // 1 year
+            });
+            stdout.pipe(res);
           });
-          stdout.pipe(res);
-        });
-      }).on('error', next);
+        }).on('error', next);
+
+      // Timeout after five seconds. Better luck next time.
+      request.setTimeout(5000, function () {
+        return res.send(504);
+      });
     };
 
   // Validate query string parameters.
